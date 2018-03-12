563dc54838f98eec0e781ae742a133c6a078478c
Abdul Haseeb <Abdulhasiib@live.com>
2018-03-12 08:04:15 +0530

    Nullable types and null coalescing operator

--- PracticeCSharp/Class1.cs
+++ PracticeCSharp/Class1.cs
@@ -10,13 +10,44 @@ namespace PracticeCSharp
     {
         static void Main()
         {
+            //Reading and Writing to Console Application
             Console.WriteLine("Enter your First Name: ");
             string FirstName = Console.ReadLine();
 
             Console.WriteLine("Enter your Last Name: ");
             string LastName = Console.ReadLine();
 
-            Console.WriteLine("Hello! Mr. {0} {1}", FirstName, LastName);
+            //Escape Sequence 
+            Console.WriteLine("Hello! \nMr. {0} {1}", FirstName, LastName);
+            
+            //Escape Sequnce and Verbatim Literal
+            string MyCSharpCodePath = @"C:\Users\muHtasib\source\repos\PracticeCSharp\PracticeCSharp";
+            Console.WriteLine("Your C# Project path is \n{0}", MyCSharpCodePath);
+
+            //Nullable types
+            bool? IsMarried = null;
+
+            if (IsMarried == true)
+            {
+                Console.WriteLine("Congratulations!");
+            }
+            else if (IsMarried == false)
+            {
+                Console.WriteLine("Cool! Enjoy freedom ;) ");
+            }
+            else
+            {
+                Console.WriteLine("Wanna keep it secret! All right");
+            }
+
+            //null coalescinf operator
+            int Income;
+            int? Salary = null;
+
+            Income = Salary ?? 0;
+
+            Console.WriteLine("Your income is {0}", Income);
+
             Console.ReadLine();
         }
     }
